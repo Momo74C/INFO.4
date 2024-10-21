f0d031a97e0637062092a7b9613241ce0f596242
Momo74C <150558954+Momo74C@users.noreply.github.com>
2024-10-21 11:05:15 +0000

    done

--- Info.C/conta.c
+++ Info.C/conta.c
@@ -3,31 +3,29 @@
 int main() 
 {
     FILE* file1 = fopen("file.txt", "r");
-    FILE* file2 = fopen("momo.txt", "w");
-    if (file1 == NULL || file2 == NULL) 
+    if (file1 == NULL) 
     {
         perror("Errore durante l'apertura del file");
     }
-
      
     int contR=0;
     int contC=0;
 
-    char riga[100]; 
+    char riga[50]; 
     while (fgets(riga, sizeof(riga), file1) != NULL) 
     {
-        for(int i=0;i<100;i++)
+        for(int i=0;i<100 && i != ' ';i++)
         {   
             contC++;
         }
         contR++;
     }
-    printf("il numero di righe e' %d\n", contR);
     printf("il numero di caratterri e' %d\n", contC);
+    printf("il numero di righe e' %d\n", contR);
+    
 
 
     fclose(file1);
-    fclose(file2);
 
     return 0;
 }
