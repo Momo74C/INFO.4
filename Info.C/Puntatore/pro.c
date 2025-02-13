e8c48ae32f10a564c8627daa12304ca8e1d02025
Momo74C <150558954+Momo74C@users.noreply.github.com>
2025-02-13 10:40:07 +0000

    update

--- Info.C/Puntatore/pro.c
+++ Info.C/Puntatore/pro.c
@@ -1,54 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 5
 
-void Carica(int v[],int *p);
-void Stampa(int v[],int *p);
-int Max(int v[],int *p);
+void Carica(int v[]);
+void Stampa(int v[]);
+int *Max(int v[]);
+int *Min(int v[]);
 
 int main ()
 {
+    srand(time(NULL));
     int vet[N];
-    int *a;
-    a = vet;
 
-    Carica(vet,a);
-    Stampa(vet,a);
+    Carica(vet);
+    Stampa(vet);
 
-    int r = Max(vet,a);
-    printf("L'elemento massimo e' : %d\n", r);
+    int *r = Max(vet);
+    printf("L'indirizzo %d, indice %ld, valore %d\n", r, r-vet,*r);
+
+    *r = Min(vet);
+    printf("L'indirizzo %d, indice %ld, valore %d\n", r, r-vet,*r);
+    
 }
 
-void Carica(int v[],int *p)
+void Carica(int v[])
 {
+    int *p;
     printf("inserici i numeri\n");
     for(p=v;p-v<N;p++)
     {
-        scanf("%d", p);
+        *p = rand () % 99 + 1;
     }
 }
 
-void Stampa(int v[],int *p)
+void Stampa(int v[])
 {
+    int *p;
     for(p=v;p-v<N;p++)
     {
         printf("Il numero e': %d\n", *p);
     }
 }
 
-int Max(int v[], int *p) 
+int *Max(int v[]) 
 {
-    int *max = v;  
-    int *ptr;  
+    int *p_max = v;  
+    int *p;
 
-    for (ptr = v; ptr - v < N; ptr++) 
+    for (p = v; p - v < N; p++) 
     {
-        if (*ptr > *max) 
+        if (*p > *p_max) 
         {
-            max = ptr;
+            p_max = p;
         }
     }
-
-    *p = max - v;
-    printf("L'indice del elemento massimo e' : %d\n", *p); 
-    return *max;
+    return p_max;
 }
+
+/**
+ * @brief restituisce il puntatore 
+ * all'elemento che contiene il valore minimo dell'array
+ */
+int *Min(int v[])
+{
+    int *p_min = v;  
+    int *p;
+
+    //scorro il puntatore per tutto l'array v
+    for (p=v; p-v<N; p++) 
+    {
+        //se il valore puntato da p è 
+        //il minore trovato fino ad adesso
+        if (*p < *p_min) 
+        {
+            //Aggiorno l'indirizzo al valore minimo
+            p_min = p;
+        }
+    }
+    return p_min;
+}
+
